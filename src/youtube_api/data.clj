ebe5deaf0fea68baf766ebae807a110aaa379095
Mike Flynn <mike@metacafe.com>
2013-02-14 16:21:02 -0800

    master: Updated fetching logic in data to be a little more flexible.

--- src/youtube_api/data.clj
+++ src/youtube_api/data.clj
@@ -4,18 +4,36 @@
 
 (def base-url "https://www.googleapis.com/youtube/v3")
 
-(defn fetch [endpoint params limit headers private data]
+(defn fetch [endpoint params headers private]
   (if-let [body (apicore/api-request (str base-url endpoint)
-                                     (assoc params "maxResults" (if (< limit 50) limit 50))
+                                     params
                                      headers
                                      private)]
-    (let [items (get body "items")
-          return (if (nil? items) data (apply conj data items))
-          nextPageToken (if (< (count items) limit) (get body "nextPageToken") nil)]
-      (if (nil? nextPageToken)
-        return
-        (fetch endpoint (assoc params "pageToken" nextPageToken) (- limit (count items)) headers private return)))
+    body
     (throw (Exception. "Error: Failed API request."))))
+
+(defn fetch-list [endpoint params headers private limit & [data]]
+"Recursive fetch function based on the 'items' field in the API response"
+  ;(println (str "Limit: " limit "\n"))
+  (let [per (if (nil? limit) 25 (if (< limit 50) limit 50))
+        body (fetch endpoint (assoc params "maxResults" per) headers private)]
+    (if-let [items (get body "items")]
+      (let [return (apply conj (if (nil? data) [] data) (if (nil? items) [] items))
+            nextPageToken (get body "nextPageToken")]
+        ;(println (str "Result Count: " (count items) "\n"))
+        ;(println (str "Next Page: " nextPageToken "\n"))
+        (if (nil? nextPageToken)
+            return
+            (fetch-list endpoint
+                        (assoc params "pageToken" nextPageToken)
+                        headers
+                        private
+                        (- limit (count items))
+                        return)))
+    (do
+      (println "ERROR: Failed to get valid list API response.")
+      (println body)
+      data))))
 
 ; API endpoint functions
 
@@ -25,7 +43,7 @@
 "https://developers.google.com/youtube/v3/docs/channels/list"
   (if (and (nil? categoryId) (nil? id) (nil? mine) (nil? mySubscribers))
     (throw (Exception. "Error: Filter param missing (id, categoryId, mine mySubscribers)!"))
-    (fetch "/channels" {"part" parts "id" id "mine" mine "mySubscribers" mySubscribers} maxResults {} true [])))
+    (fetch-list "/channels" {"part" parts "id" id "mine" mine "mySubscribers" mySubscribers} {} true maxResults [])))
 
 (defn activities-list [parts & {:keys [channelId home mine publishedAfter publishedBefore maxResults]
                               :or {channelId nil home nil mine nil publishedAfter nil publishedBefore nil maxResults 5}
@@ -33,12 +51,12 @@
 "https://developers.google.com/youtube/v3/docs/activities/list"
   (if (and (nil? channelId) (nil? home) (nil? mine))
     (throw (Exception. "Error: Filter param missing (channelId, home, mine)!"))
-    (fetch "/activities" {"part" parts
+    (fetch-list "/activities" {"part" parts
                                      "channelId" channelId
                                      "home" home
                                      "mine" mine
                                      "publishedAfter" (if (nil? publishedAfter) publishedAfter (apicore/date_format_iso8601 publishedAfter))
-                                     "publishedBefore" (if (nil? publishedBefore) publishedBefore (apicore/date_format_iso8601 publishedBefore))} maxResults {} true [])))
+                                     "publishedBefore" (if (nil? publishedBefore) publishedBefore (apicore/date_format_iso8601 publishedBefore))} {} true maxResults [])))
 
 (defn guideCatagories-list [parts & {:keys [id regionCode hl maxResults]
                               :or {id nil regionCode nil hl nil maxResults 5}
@@ -46,9 +64,9 @@
 "https://developers.google.com/youtube/v3/docs/guideCategories/list"
   (if (and (nil? id) (nil? regionCode))
     (throw (Exception. "Error: Filter param missing (id, regionCode)!"))
-    (fetch "/guideCategories" {"part" parts
+    (fetch-list "/guideCategories" {"part" parts
                                "id" id
-                               "regionCode" regionCode} maxResults {} true [])))
+                               "regionCode" regionCode} {} true maxResults [])))
 
 (defn playlistItems-list [parts & {:keys [id playlistId maxResults]
                               :or {id nil playlistId nil maxResults 5}
@@ -56,9 +74,9 @@
 "https://developers.google.com/youtube/v3/docs/playlistItems/list"
   (if (and (nil? id) (nil? playlistId))
     (throw (Exception. "Error: Filter param missing (id, playlistId)!"))
-    (fetch "/playlistItems" {"part" parts
+    (fetch-list "/playlistItems" {"part" parts
                                              "id" id
-                                             "playlistId" playlistId} maxResults {} true [])))
+                                             "playlistId" playlistId} {} true maxResults [])))
 
 (defn playlists-list [parts & {:keys [id channelId mine maxResults]
                               :or {id nil playlistId nil mine nil maxResults 5}
@@ -66,10 +84,10 @@
 "https://developers.google.com/youtube/v3/docs/playlists/list"
   (if (and (nil? id) (nil? channelId) (nil? mine))
     (throw (Exception. "Error: Filter param missing (channelId, id, playlistId)!"))
-    (fetch "/playlists" {"part" parts
+    (fetch-list "/playlists" {"part" parts
                                              "channelId" channelId
                                              "id" id
-                                             "mine" mine} maxResults {} true [])))
+                                             "mine" mine} {} true maxResults [])))
 
 (defn subscriptions-list [parts & {:keys [id channelId mine forChannelId order maxResults]
                               :or {id nil playlistId nil mine nil forChannelId nil order "alphabetical" maxResults 5}
@@ -77,12 +95,12 @@
 "https://developers.google.com/youtube/v3/docs/subscriptions/list"
   (if (and (nil? id) (nil? channelId) (nil? mine))
     (throw (Exception. "Error: Filter param missing (channelId, id, mine)!"))
-    (fetch "/subscriptions" {"part" parts
+    (fetch-list "/subscriptions" {"part" parts
                                            "channelId" channelId
                                            "id" id
                                            "mine" mine
                                            "order" (if (= -1 (.indexOf '("alphabetical" "relevance" "unread") order)) "alphabetical" order)
-                                           "forChannelId" forChannelId} maxResults {} true [])))
+                                           "forChannelId" forChannelId} {} true maxResults [])))
 
 (defn videoCategories-list [parts & {:keys [id regionCode hl maxResults]
                               :or {id nil regionCode nil hl nil maxResults 5}
@@ -90,13 +108,13 @@
 "https://developers.google.com/youtube/v3/docs/videoCategories/list"
   (if (and (nil? id) (nil? regionCode))
     (throw (Exception. "Error: Filter param missing (id, regionCode)!"))
-    (fetch "/videoCategories" {"part" parts
+    (fetch-list "/videoCategories" {"part" parts
                                              "id" id
-                                             "regionCode" regionCode} maxResults {} true [])))
+                                             "regionCode" regionCode}  {} true maxResults [])))
 
 (defn videos-list [parts id]
 "https://developers.google.com/youtube/v3/docs/videos/list"
-    (fetch "/videos" {"part" parts "id" id} nil {} true []))
+    (fetch-list "/videos" {"part" parts "id" id} {} true nil []))
 
 (defn search-list [parts & {:keys [relatedToVideoId
                                    channelId
@@ -136,22 +154,22 @@
                                    maxResults 5}
                               :as argmap}]
 "https://developers.google.com/youtube/v3/docs/search/list"
-    (fetch "/search" {"part" parts
-                                    "relatedToVideoId" relatedToVideoId
-                                    "channelId" channelId
-                                    "order" order
-                                    "publishedAfter" (if (nil? publishedAfter) publishedAfter (apicore/date_format_iso8601 publishedAfter))
-                                    "publishedBefore" (if (nil? publishedBefore) publishedBefore (apicore/date_format_iso8601 publishedBefore))
-                                    "q" q
-                                    "regionCode" regionCode
-                                    "topicId" topicId
-                                    "type" type
-                                    "videoCaption" (if (= -1 (.indexOf '("any" "closedCaption" "none") videoCaption)) "any" videoCaption)
-                                    "videoCategoryId" nil
-                                    "videoDefinition" (if (= -1 (.indexOf '("any" "high" "standard") videoCaption)) "any" videoCaption)
-                                    "videoDimension" (if (= -1 (.indexOf '("any" "2d" "3d") videoCaption)) "any" videoCaption)
-                                    "videoDuration" (if (= -1 (.indexOf '("any" "long" "medium" "short") videoCaption)) "any" videoCaption)
-                                    "videoEmbeddable" (if (= -1 (.indexOf '("any" "true") videoCaption)) "any" videoCaption)
-                                    "videoLicense" (if (= -1 (.indexOf '("any" "creativeCommon" "youtube") videoCaption)) "any" videoCaption)
-                                    "videoSyndicated" (if (= -1 (.indexOf '("any" "true") videoCaption)) "any" videoCaption)
-                                    } maxResults {} true []))
+    (fetch-list "/search" {"part" parts
+                           "relatedToVideoId" relatedToVideoId
+                           "channelId" channelId
+                           "order" order
+                           "publishedAfter" (if (nil? publishedAfter) publishedAfter (apicore/date_format_iso8601 publishedAfter))
+                           "publishedBefore" (if (nil? publishedBefore) publishedBefore (apicore/date_format_iso8601 publishedBefore))
+                           "q" q
+                           "regionCode" regionCode
+                           "topicId" topicId
+                           "type" type
+                           "videoCaption" (if (= -1 (.indexOf '("any" "closedCaption" "none") videoCaption)) "any" videoCaption)
+                           "videoCategoryId" nil
+                           "videoDefinition" (if (= -1 (.indexOf '("any" "high" "standard") videoCaption)) "any" videoCaption)
+                           "videoDimension" (if (= -1 (.indexOf '("any" "2d" "3d") videoCaption)) "any" videoCaption)
+                           "videoDuration" (if (= -1 (.indexOf '("any" "long" "medium" "short") videoCaption)) "any" videoCaption)
+                           "videoEmbeddable" (if (= -1 (.indexOf '("any" "true") videoCaption)) "any" videoCaption)
+                           "videoLicense" (if (= -1 (.indexOf '("any" "creativeCommon" "youtube") videoCaption)) "any" videoCaption)
+                           "videoSyndicated" (if (= -1 (.indexOf '("any" "true") videoCaption)) "any" videoCaption)
+                           } {} true maxResults []))
